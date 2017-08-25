282fb7c0fa77ffef1ef815f1f195453ae51f60a3
OMooshroom <mooshroom@yeah.net>
2017-08-25 11:18:36 +0800

    update: 10_属性

--- 10_属性/10_属性/main.swift
+++ 10_属性/10_属性/main.swift
@@ -48,3 +48,121 @@ print(manager.importer.fileName)
 
 
 //: 计算属性
+// 它并不存储值,相反的是它能提供一个读取器和一个可选的设置其来间接的设置其他的属性和值
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set {
+            origin.x = newValue.x - (size.width / 2)
+            origin.y = newValue.y - (size.height / 2)
+        }
+    }
+}
+var square = Rect(origin: Point(x: 0.0, y: 0.0), size: Size(width: 10.0, height: 10.0))
+let initialSquareCenter = square.center
+square.center = Point(x: 15.0, y: 15.0)
+print(square.origin.x, square.origin.y)
+
+
+
+//: 只读计算属性
+struct Person {
+    var firstName = "Moo"
+    var lastName = "Shroom"
+    var name: String {
+        return firstName + lastName
+    }
+}
+let moo = Person()
+//moo.name = "123"   error: only-read
+print("His name is \(moo.name)")
+
+
+
+
+//: 属性观察者
+// willset 会在该值被存储之前被调用
+// didset 会在新值被存储后被调用
+class StepCounter {
+    var totalSteps: Int = 0 {
+        willSet(newTotalSteps) {
+            print("About to set totalSteps to \(newTotalSteps)")
+        }
+        didSet {
+            if totalSteps > oldValue {
+                print("Added \(totalSteps - oldValue) steps")
+            }
+        }
+    }
+}
+
+let stepCount = StepCounter()
+stepCount.totalSteps = 200
+stepCount.totalSteps = 300
+stepCount.totalSteps = 400
+
+
+
+//: 全局变量
+// 同样拥有 willSet 和 didSet
+var name: String = "" {
+    willSet {
+        print("new " + newValue)
+    }
+    didSet {
+        print("old " + oldValue)
+    }
+}
+
+name = "1"
+name = "2"
+
+
+
+
+//: 类型属性
+// 对于struct、enum 我们使用static来设置类型属性
+// 对于class 我们使用static或者class来设置类型属性，class关键字允许子类重写父类的实现。
+struct SomeStructure {
+    static var storedTypeProperty = "some value"
+    static var computedTypeProperty: Int {
+        return 1
+    }
+}
+enum SomEnumeration {
+    static var storedTypeProperty = "some value"
+    static var computedTypeProperty: Int {
+        return 3
+    }
+}
+class SomeClass {
+    static var storedTypeProperty = "some value"
+    static var computedTypeProperty: Int {
+        return 7
+    }
+    class var overrideableComputedTypeProperty: Int {
+        return 107
+    }
+}
+class SubSomeClass: SomeClass {
+    class override var overrideableComputedTypeProperty: Int {
+        return 108
+    }
+}
+print(SomeClass.overrideableComputedTypeProperty)
+print(SubSomeClass.overrideableComputedTypeProperty)
+
+
+
